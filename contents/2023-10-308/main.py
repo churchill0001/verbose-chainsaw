95bfa1b45efd6bf12881db66b011b362293b80f3
Churchill <135497026+churchill0001@users.noreply.github.com>
2023-10-30 14:36:00 +0700

    Updates

---
+++ contents/2023-10-308/main.py
@@ -0,0 +1,55 @@
+import os
+import json
+
+
+data = 84
+value = 88
+result = 95
+number = 46
+total = 11
+
+
+
+def dataprint_rvd(x, y):
+    print('exgab')
+    for value in range(5):
+        print(value)
+
+dataprint_rvd(3, 5)
+
+def printprint_upg(x, y):
+    print('iunsf')
+    for total in range(5):
+        print(total)
+    print('evxth')
+    total = 0
+    while total < 5:
+        print(total)
+        total += 1
+
+printprint_upg(3, 5)
+
+def printdata_yjz(n):
+    if n <= 0:
+        return 0
+    print('nltrh')
+    print(number)
+    total = 0
+    while total < 5:
+        print(total)
+        total += 1
+    return 60
+
+result = printdata_yjz(10)
+print('Result of recursive function:', result)
+
+
+def main():
+    print('sxxxz')
+    print(number)
+    if fetchdata_jep():
+        print('Conditional block')
+    return 22
+print('Return value of main function:', main())
+if __name__ == '__main__':
+    main()
